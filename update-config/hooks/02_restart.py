7d07a9b254541b5b2fc198efc6f11ee264e9fb9e
Mathieu Pasquet <mathieu@pasquet.co>
2020-04-23 21:50:11 +0100

    Fix fstring issue

--- update-config/hooks/02_restart.py
+++ update-config/hooks/02_restart.py
@@ -14,4 +14,5 @@ with open('/config/changes.txt', 'r') as changes:
       os.popen(f'docker-compose -f /config/compose/docker-compose.yaml \
        up -d --remove-orphans {service}')
     elif path.split('/')[0] in containers:
-      os.popen(f'docker restart {path.split('/')[0]}')
+      container = path.split('/')[0]
+      os.popen(f'docker restart {container}')
