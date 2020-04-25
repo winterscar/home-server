c5623768a9fcc0b4f13a4c712161a6abbf612b3c
Mathieu Pasquet <mathieu@pasquet.co>
2020-04-26 00:55:20 +0100

    Stop using generators

--- update-config/hooks/02_restart.py
+++ update-config/hooks/02_restart.py
@@ -2,8 +2,9 @@
 import os
 from pathlib import Path
 
-containers = map(str.rstrip, os.popen("docker ps --format '{{.Names}}'").readlines())
-with open('/config/changes.txt', 'r') as changes:
+containers = {c.rstrip() for c in os.popen("docker ps --format '{{.Names}}'").readlines()}
+with open('/config/changes.txt', 'r') as f:
+  changes = [line.strip() for line in f]
   rebuilds = {c.stem for r in changes if (c := Path(r)).parts[0] == 'compose'}
   reloads  = {c for r in changes if (c := Path(r).parts[0]) in containers}
 
