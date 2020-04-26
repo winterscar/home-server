89e24d9919de3c1ba8ce89778de9b61a3c431718
Mathieu Pasquet <mathieu@pasquet.co>
2020-04-26 22:00:26 +0100

    Get changes from payload, not from pull

--- update-config/hooks/00_pull.sh
+++ update-config/hooks/00_pull.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-
-cd /config
-git fetch
-git diff --name-only master origin/master > changes.txt
-git pull
+git -C /config pull
--- update-config/hooks/02_restart.py
+++ update-config/hooks/02_restart.py
@@ -2,21 +2,26 @@
 import os
 from pathlib import Path
 from subprocess import check_call, check_output
+from sys import argv
+import json
 
 # docker commands
 d_ps = ['docker', 'ps', '--format', '{{.Names}}']
-d_compose = ['docker-compose', '-f',  '/config/compose/docker-compose.yaml',
-                              '--project-directory=/config/compose'
-                              'up', '-d', '--remove-orphans']
+d_compose = ['docker-compose',
+            '-f',  '/config/compose/docker-compose.yaml',
+            '--project-directory=/config/compose'
+            'up', '-d', '--remove-orphans']
 d_restart = ['docker', 'restart']
 
 containers = {c for c in check_output(d_ps, text=True).split('\n')}
-with open('/config/changes.txt', 'r') as f:
-  changes = [line.strip() for line in f]
-  rebuilds = {c.stem for r in changes if (c := Path(r)).parts[0] == 'compose'}
-  reloads  = {c for r in changes if (c := Path(r).parts[0]) in containers}
 
-  if rebuilds:
-    check_call(d_compose)
-  for container in reloads - rebuilds:
-    check_call(d_restart + container)
+payload = json.loads(argv[1])
+changes = [a for v in [f['modified'] + f['added'] for f in payload['commits']] for a in v]
+
+rebuilds = {c.stem for r in changes if (c := Path(r)).parts[0] == 'compose'}
+reloads  = {c for r in changes if (c := Path(r).parts[0]) in containers}
+
+if rebuilds:
+  check_call(d_compose)
+for container in reloads - rebuilds:
+  check_call(d_restart + container)
