57eb93cd57189338998ab3cf47f45ab336a44341
Mathieu Pasquet <mathieu@pasquet.co>
2020-04-26 20:14:12 +0100

    Run restart script synchronously

--- update-config/hooks/02_restart.py
+++ update-config/hooks/02_restart.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 import os
 from pathlib import Path
+from subprocess import check_call, check_output
 
-containers = {c.rstrip() for c in os.popen("docker ps --format '{{.Names}}'").readlines()}
+# docker commands
+d_ps = ['docker', 'ps', '--format', '{{.Names}}']
+d_compose = ['docker-compose', '-f',  '/config/compose/docker-compose.yaml',
+                              '--project-directory=/config/compose'
+                              'up', '-d', '--remove-orphans']
+d_restart = ['docker', 'restart']
+
+containers = {c for c in check_output(d_ps, text=True).split('\n')}
 with open('/config/changes.txt', 'r') as f:
   changes = [line.strip() for line in f]
   rebuilds = {c.stem for r in changes if (c := Path(r)).parts[0] == 'compose'}
   reloads  = {c for r in changes if (c := Path(r).parts[0]) in containers}
 
   if rebuilds:
-    os.popen(f'docker-compose -f /config/compose/docker-compose.yaml \
-                              --project-directory=/config/compose \
-                              up -d --remove-orphans')
+    check_call(d_compose)
   for container in reloads - rebuilds:
-    os.popen(f'docker restart {container}')
+    check_call(d_restart + container)
