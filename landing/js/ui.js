ee17ef8f6f8a9e88032d428ed1e94d6b346f3248
Mathieu Pasquet <mathieu@pasquet.co>
2020-05-01 14:14:48 +0100

    Add auto-complete functionality to search, autofocus input, search google

--- landing/js/ui.js
+++ landing/js/ui.js
@@ -1,8 +1,47 @@
 async function PullBackground() {
 	let c = ['au', 'ca', 'cn', 'de', 'fr', 'jp', 'us', 'gb'][Math.floor(Math.random()*8)]
 	var json = await (await fetch(`https://peapix.com/bing/feed?country=${c}`)).json()
-	document.body.style.backgroundImage = "url("+ json[0].fullUrl +")"
-	document.body.style.backgroundSize = "100%";
+	$('#background').attr("src", json[0].fullUrl)
+}
+
+async function GetTopSuggestion(seed) {
+	let r = await fetch("https://landingautosuggest.cognitiveservices.azure.com/bing/v7.0/suggestions?q="+ seed, {
+		"method": "GET",
+		"headers": {
+			"content-type": "application/json",
+			"bingapis-market": "en-UK",
+			"ocp-apim-subscription-key": "e0d5e841ab604b6c94bc4aa822b3be00"
+		},
+	})
+	let results = (await r.json()).suggestionGroups[0].searchSuggestions
+	results = results.map(r => r.displayText)
+	return (results.length) > 0 ? results : []
+}
+
+var lastQuery = Date.now() - 2000;
+var pv;
+
+const UpdateSearch = async query => {
+	let now = Date.now()
+	if (now - lastQuery > 1000){
+		lastQuery = now
+		pv.options = await GetTopSuggestion(query)
+	}
+	pv.repaint()
+	pv.o3DropDown();
+}
+
+function SetupTabCompletion(){
+	pv = completely(document.getElementById('search'),{
+		backgroundColor:'transparent',
+		fontFamily: 'cascadia code, monospace',
+		fontSize: '13px',
+		color:'#DDDDDD',
+		overflow: 'hidden'
+	});
+	pv.input.focus();
+	pv.onChange = UpdateSearch
+	pv.onEnter = () => window.location = 'https://google.com/search?q=' + pv.getText()
 }
 
 // postition won't work correctly if you
@@ -10,6 +49,7 @@ async function PullBackground() {
 // the browsers default content display.
 // UI mods should not affect it.
 $(document).ready(function() {
+	SetupTabCompletion();
 	PullBackground();
 	$( "#container" ).draggable().position({
 	my: "center",
