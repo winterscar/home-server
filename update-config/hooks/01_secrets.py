69c75eee896fd1ead8c6cc76a71b1b2ebb4e3f10
Mathieu Pasquet <mathieu@pasquet.co>
2021-01-21 19:01:17 +0000

    Pipeline: Refactor config path, fix key error

--- update-config/hooks/01_secrets.py
+++ update-config/hooks/01_secrets.py
@@ -5,16 +5,16 @@ import re
 
 
 placeholder = re.compile(r"\$\{(?P<key>.+?)\}")
+root = "/config/"
 
 with open("/config/secrets.yaml", 'r') as stream:
   secrets = yaml.safe_load(stream)
 
 for filepath in secrets:
-  filepath = "/config/" + filepath
-  with open(filepath, 'r') as config:
+  with open(root + filepath, 'r') as config:
     configdata = config.read()
   for key in secrets[filepath]:
     configdata = configdata.replace(r"${" + key + r"}", str(secrets[filepath][key]))
-  with open(filepath, 'w') as config:
+  with open(root + filepath, 'w') as config:
     config.write(configdata)
   print("replaced secrets in " + filepath)
