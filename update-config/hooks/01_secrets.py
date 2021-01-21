cc36d96979b78e97534843def4d0499dce09d601
Mathieu Pasquet <mathieu@pasquet.co>
2021-01-21 18:09:43 +0000

    Pipeline: Add secrets processing

---
+++ update-config/hooks/01_secrets.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import yaml
+import os
+import re
+
+
+placeholder = re.compile(r"\$\{(?P<key>.+?)\}")
+
+with open("/config/secrets.yaml", 'r') as stream:
+  secrets = yaml.safe_load(stream)
+
+for filepath in secrets:
+  with open(filepath, 'r') as config:
+    configdata = config.read()
+  for keys in secrets[filepath]:
+    configdata = configdata.replace(r"${" + key + r"}", str(secrets[filepath][key]))
+  with open(filepath, 'w') as config:
+    config.write(configdata)
+  print("replaced secrets in " + filepath)
